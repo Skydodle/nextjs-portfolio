cca3160c357ce9ef5ec0e1fa200b583ce55f549b
skydodle <skydodle@gmail.com>
2022-10-06 20:12:00 -0700

    Added Navbar containers css and logo image

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src="/../public/assets/logo.png"
+          alt="/"
+          width="180"
+          height="140"
+        />
+        <div>Navbar</div>
+        <div>Hello</div>
+        <div>test</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
