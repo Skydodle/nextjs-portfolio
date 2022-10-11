91c728fe5a2c7b5806a1c81d12e4f3a123c57ba0
skydodle <skydodle@gmail.com>
2022-10-11 15:53:21 -0700

    Added hero text and social icons

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from 'react-icons/ai';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const Main = () => {
   return (
@@ -12,6 +15,27 @@ const Main = () => {
             Hi, I'm <span className='text-[#5651e5]'>Johnny</span>
           </h1>
           <h1 className='py-2 text-gray-700'>A Front-End Web Developer</h1>
+          <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
+            I'm a front-end web developer specialing in building and desigingin
+            exceptional digital experience. Currently, I'm focused on building
+            responsive front-end web applications while learning back-end
+            technologies.
+          </p>
+          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+            <div></div>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <FaLinkedinIn />
+            </div>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <FaGithub />
+            </div>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <AiOutlineMail />
+            </div>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <BsFillPersonLinesFill />
+            </div>
+          </div>
         </div>
       </div>
     </div>
